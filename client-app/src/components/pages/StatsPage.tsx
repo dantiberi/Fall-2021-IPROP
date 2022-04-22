3145c57a0b76298ecfcb101c11ab0d7f4e9e15c5
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2022-04-22 09:36:17 -0500

    Fetch statistics and display them on StatsPage

--- client-app/src/components/pages/StatsPage.tsx
+++ client-app/src/components/pages/StatsPage.tsx
@@ -7,109 +7,156 @@ import Select from "@mui/material/Select";
 import { SelectChangeEvent } from "@mui/material/Select/SelectInput";
 
 import getAzureFunctions from "getAzureFunctions";
-
-const stats = [
-    {
-        subject: "exponent",
-        values: [{ name: "Correct", score: 6 },
-            { name: "Incorrect", score: 4 }]
-    },
-    {
-        subject: "geometry",
-        values: [{name: "Correct", score: 5 },
-                { name: "Incorrect", score: 5 },]
-    },
-    {
-        subject: "trigonometry",
-        values: [{name: "Correct", score: 3 },
-                { name: "Incorrect", score: 7 },]
-    },
-];
-
-const students = [
-    "Student 1",
-    "Student 2",
-    "Student 3"
-]
-const subjects = [
-    "exponent",
-    "geometry",
-    "trigonometry"
-]
+import SubjectModel from "models/SubjectModel";
+import useFetch, { FetchStatus } from "hooks/useFetch";
+import { isStatisticModel } from "models/StatisticModel";
+import LoadingAnimation from "components/LoadingAnimation";
 
 interface statProps {
-    instructor_id : number
+    instructor_id: number,
+    subjects: SubjectModel[]
 }
+
 const StatsPage: React.FC<statProps> = (props) => {
-    const url = new URL(getAzureFunctions().GetStatistic);
-        // Store course code in a variable since we use it later (and it may change since it's after the PUT request)
+    const [subjectName, setSubjectName] = useState("");
+    const [studentEmail, setStudentEmail] = useState("");
+
+    const url = new URL(getAzureFunctions().GetStatistics);
     url.searchParams.append("instructor_id", props.instructor_id.toString());
-
-    const requestInfo: RequestInit = { method: "GET" };
-
-    fetch(url.toString(), requestInfo)
-        .then(response => {
-            console.log(response);
-        })
-        .then(json => {
-            console.log(json);
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    const [subject, setSubject] = useState("")
-    const [student, setStudent] = useState("")
-
-    const handleChangeSubject= (event: SelectChangeEvent) => {
-        setSubject(event.target.value);
-    };
-    const handleChangeStudent= (event: SelectChangeEvent) => {
-        setStudent(event.target.value);
-    };
-    return (
-        <div>
-            <span>Subject</span>
-            <Select
-                label="Subject"
-                style={{width: 80}}
-                onChange={handleChangeSubject}
-                >
-                <MenuItem value="">
-                    <em>None</em>
-                </MenuItem>
-                {subjects.map((subjectItem) =>(
-                    <MenuItem value={subjectItem}>{subjectItem}</MenuItem>
-                ))}
-            </Select>
-
-            <span>Student</span>
-            <Select
-                label="Student"
-                style={{width: 80}}
-                onChange={handleChangeStudent}
-                >
-                <MenuItem value="">
-                    <em>None</em>
-                </MenuItem>
-                {students.map((studentItem) =>(
-                    <MenuItem value={studentItem}>{studentItem}</MenuItem>
-                ))}
-            </Select>
-
-            {(student === ""? students : students.filter((studentItem) => studentItem === student)).map((value, index)=>(
-                <Grid container>
-                <Grid item xs={3}>
-                    <p>{value}</p>
-                </Grid>
-                <Grid item xs={9}>
-                    <PieCharts data={subject === ""? stats :stats.filter((subjectItem) => (subjectItem.subject === subject))} name={"name"} value={"score"}/>
-                </Grid>
-
-                </Grid>
-            ))}
-
-        </div>
+    
+    const statsFetchResult = useFetch(
+        url.toString(),
+        (data) => {
+            // The Azure function should return the data as an array of SubjectModels
+            if (Array.isArray(data) && data.every(isStatisticModel)) {
+                return data;
+            }
+            return undefined;
+        },
+        [props.instructor_id]
     );
+
+    const handleChangeSubjectName = (event: SelectChangeEvent) => {
+        setSubjectName(event.target.value);
+    };
+    const handleChangeStudentEmail = (event: SelectChangeEvent) => {
+        setStudentEmail(event.target.value);
+    };
+
+    if (statsFetchResult.status === FetchStatus.Success) {
+        const stats = statsFetchResult.payload;
+
+        const studentEmails = stats
+            .map(stat => stat.email)
+            .filter((email, index, array) => array.indexOf(email) === index);
+
+        if (studentEmail !== "" && !studentEmails.includes(studentEmail)) {
+            setStudentEmail("");
+        }
+
+        const statMap = new Map<string, Map<string, [number, number]>>();
+
+        const relevantStats = stats
+            .filter(stat => (studentEmail === "") || (stat.email === studentEmail))
+            .filter(stat => (subjectName === "") || (stat.subject_name === subjectName))
+
+        relevantStats.forEach(stat => {
+            let emailStats = statMap.get(stat.email)
+
+            if (emailStats === undefined) {
+                emailStats = new Map<string, [number, number]>();
+                statMap.set(stat.email, emailStats);
+            }
+
+            const oldValue = emailStats.get(stat.subject_name);
+            if (oldValue === undefined) {
+                emailStats.set(stat.subject_name, [stat.correct_attempt, stat.incorrect_attempt]);
+            } else {
+                const [oldCorrect, oldIncorrect] = oldValue;
+                emailStats.set(stat.subject_name, [oldCorrect + stat.correct_attempt, oldIncorrect + stat.incorrect_attempt]);
+            }
+        });
+
+        // const [correct, incorrect] = relevantStats
+        //     .map(stat => [stat.correct_attempt, stat.incorrect_attempt])
+        //     .reduce(([prev_correct, prev_incorrect], [curr_correct, curr_incorrect]) => [
+        //         prev_correct + curr_correct, prev_incorrect + curr_incorrect
+        //     ]);
+
+        return (
+            <div>
+                <span>Subject</span>
+                <Select
+                    label="Subject"
+                    style={{width: 80}}
+                    onChange={handleChangeSubjectName}
+                    value={subjectName}
+                >
+                    <MenuItem value={""}>
+                        <em>All</em>
+                    </MenuItem>
+                    {props.subjects.map((subject) =>(
+                        <MenuItem value={subject.subject_name} key={subject.id}>{subject.subject_name}</MenuItem>
+                    ))}
+                </Select>
+    
+                <span>Student</span>
+                <Select
+                    label="Student"
+                    style={{width: 80}}
+                    onChange={handleChangeStudentEmail}
+                    value={studentEmail}
+                >
+                    <MenuItem value="">
+                        <em>All</em>
+                    </MenuItem>
+                    {studentEmails.map((email) =>(
+                        <MenuItem value={email}>{email}</MenuItem>
+                    ))}
+                </Select>
+    
+                {Array.from(statMap, ([email, emailStats]) => (
+                    <Grid container>
+                        <Grid item xs={3}>
+                            <p>{email}</p>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <PieCharts
+                                data={
+                                    Array.from(emailStats, ([subjectName, [correct, incorrect]]) => ({
+                                        subject: subjectName,
+                                        values: [
+                                            {
+                                                name: "Correct",
+                                                score: correct
+                                            },
+                                            {
+                                                name: "Incorrect",
+                                                score: incorrect
+                                            }
+                                        ]
+                                    }))
+                                }
+                                name={"name"}
+                                value={"score"}
+                            />
+                        </Grid>
+                    </Grid>
+                ))}
+            </div>
+        );
+    } else if (statsFetchResult.status === FetchStatus.InProgress) {
+        return (
+            <>
+                <p>Fetching statistics...</p>
+                <LoadingAnimation />
+            </>
+        );
+    } else {
+        return (
+            <p>Failed to fetch statistics! Reason: {statsFetchResult.reason}</p>
+        );
+    }
 };
 
 export default StatsPage;
